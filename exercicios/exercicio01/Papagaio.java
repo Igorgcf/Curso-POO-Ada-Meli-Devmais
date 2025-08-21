d5f7f41563dbf1faae01b2786082a2423bf3c581
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:24:50 -0300

    Papagaio Model

---
+++ exercicios/exercicio01/Papagaio.java
@@ -0,0 +1,11 @@
+package exercicio01;
+
+public class Papagaio implements Animal{
+    @Override
+    public void emitirSom() {
+        System.out.println("************************");
+        System.out.println("Olá, eu sou um papagaio!");
+        System.out.println("************************");
+
+    }
+}
