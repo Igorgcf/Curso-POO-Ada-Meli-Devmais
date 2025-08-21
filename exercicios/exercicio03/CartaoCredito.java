3f95d84bff14baabf49f67daa33bad4fa555707b
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:28:52 -0300

    Cartão de crédito model

---
+++ exercicios/exercicio03/CartaoCredito.java
@@ -0,0 +1,9 @@
+package exercicio03;
+
+public class CartaoCredito implements Pagamento{
+
+    @Override
+    public void pagar(double valor) {
+        System.out.println("Pagamento de R$" + valor + " realizado com Cartão de Crédito.");
+    }
+}
