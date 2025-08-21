456de9b6343320e7c9d736c46ec69d2147b1e278
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:29:06 -0300

    Dinheiro Model

---
+++ exercicios/exercicio03/Dinheiro.java
@@ -0,0 +1,8 @@
+package exercicio03;
+
+public class Dinheiro implements Pagamento{
+    @Override
+    public void pagar(double valor) {
+        System.out.println("Pagamento de R$" + valor + " realizado com Dinheiro.");
+    }
+}
