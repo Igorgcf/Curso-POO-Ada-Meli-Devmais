ec129fd89e94fa2973422ab2f94f5cb10eca5900
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:30:47 -0300

    Bicicleta Model

---
+++ exercicios/exercicio04/Bicicleta.java
@@ -0,0 +1,19 @@
+package exercicio04;
+
+public class Bicicleta implements Veiculo {
+
+    @Override
+    public void acelerar() {
+        System.out.println("Acelerando a bicicleta!");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("Freando a bicicleta!");
+    }
+
+    @Override
+    public void ligarMotor() {
+        System.out.println("Bicicleta não tem motor!");
+    }
+}
