1797b4232062cd461da297ed1d6bba944e80a2b9
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:24:19 -0300

    Cachorro Model

---
+++ exercicios/exercicio01/Cachorro.java
@@ -0,0 +1,11 @@
+package exercicio01;
+
+public class Cachorro implements Animal{
+
+    @Override
+    public void emitirSom() {
+        System.out.println("************************");
+        System.out.println("O cachorro faz: au au!");
+        System.out.println("************************");
+    }
+}
