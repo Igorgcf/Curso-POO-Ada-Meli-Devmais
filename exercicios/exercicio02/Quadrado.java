eca491af888d5d30ea06e54ead8f3f1d899641e3
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:26:19 -0300

    Quadrado Model

---
+++ exercicios/exercicio02/Quadrado.java
@@ -0,0 +1,15 @@
+package exercicio02;
+
+public class Quadrado implements Forma {
+
+    public Quadrado(int lado) {
+        this.lado = lado;
+    }
+
+    private int lado;
+
+    @Override
+    public int calcularArea() {
+        return lado * lado;
+    }
+}
