d3f3cc7ce9822ad027ce7b30d57697d1f7949fa7
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:24:35 -0300

    Gato Model

---
+++ exercicios/exercicio01/Gato.java
@@ -0,0 +1,10 @@
+package exercicio01;
+
+public class Gato implements Animal{
+    @Override
+    public void emitirSom() {
+        System.out.println("************************");
+        System.out.println("O gato faz: miau!");
+        System.out.println("************************");
+    }
+}
