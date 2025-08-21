25d6439a606787b4a06db687c526b2cddf63d3bd
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:32:41 -0300

    Main

---
+++ exercicios/exercicio05/Main.java
@@ -0,0 +1,21 @@
+package exercicio05;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Celular celular = new Celular();
+
+        Notificacao email = new Email();
+        Notificacao sms = new SMS();
+
+        celular.getNotificacoes().add(email);
+        celular.getNotificacoes().add(sms);
+
+        System.out.println("********************************************************************");
+        for (Notificacao notificacao : celular.getNotificacoes()) {
+            notificacao.enviarMensagem("Olá, esta é uma mensagem de teste!");
+        }
+
+        System.out.println("********************************************************************");
+    }
+}
