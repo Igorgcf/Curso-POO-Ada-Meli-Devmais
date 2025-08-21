f282dd1fe62e05f07411f21815af9cfdfa817fb9
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:26:34 -0300

    Desenho Model

---
+++ exercicios/exercicio02/Desenho.java
@@ -0,0 +1,21 @@
+package exercicio02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Desenho {
+
+    List<Forma> formas;
+
+    public Desenho() {
+        this.formas = new ArrayList<>();
+    }
+
+    double calcularAreaTotal(){
+        double areaTotal = 0.0;
+        for (Forma forma : formas) {
+            areaTotal += forma.calcularArea();
+        }
+        return areaTotal;
+    }
+}
