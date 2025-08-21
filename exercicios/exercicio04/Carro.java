693debb961b9d840d1ffb891640718c58ad02f5e
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:30:21 -0300

    Carro Model

---
+++ exercicios/exercicio04/Carro.java
@@ -0,0 +1,19 @@
+package exercicio04;
+
+public class Carro implements Veiculo {
+
+    @Override
+    public void acelerar() {
+        System.out.println("Acelerando o carro!");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("Freando o carro!");
+    }
+
+    @Override
+    public void ligarMotor() {
+        Veiculo.super.ligarMotor();
+    }
+}
