0295955184811441640c71784d02ee861920e1c5
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:32:03 -0300

    E-mail Model

---
+++ exercicios/exercicio05/Email.java
@@ -0,0 +1,9 @@
+package exercicio05;
+
+public class Email implements Notificacao {
+
+    @Override
+    public void enviarMensagem(String mensagem) {
+        System.out.println("Enviando email com a mensagem: " + mensagem);
+    }
+}
