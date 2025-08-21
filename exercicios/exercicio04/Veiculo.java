4da5138bcc0631fbebd58ced5395e61e00b5986e
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:30:09 -0300

    Veiculo Interface

---
+++ exercicios/exercicio04/Veiculo.java
@@ -0,0 +1,11 @@
+package exercicio04;
+
+public interface Veiculo {
+
+    void acelerar();
+    void frear();
+
+    default void ligarMotor(){
+        System.out.println("Motor ligado!");
+    }
+}
