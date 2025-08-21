8723fc9ac80593d9ebafccc0fc4ba01244897653
igorgcf <igorTecnologia@outlook.com>
2025-08-21 15:32:12 -0300

    SMS Model

---
+++ exercicios/exercicio05/SMS.java
@@ -0,0 +1,9 @@
+package exercicio05;
+
+public class SMS implements Notificacao {
+
+    @Override
+    public void enviarMensagem(String mensagem) {
+        System.out.println("Enviando SMS com a mensagem: " + mensagem);
+    }
+}
