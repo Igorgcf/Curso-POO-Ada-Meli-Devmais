e4fbeb89083e5c49b2a9079a9e44c4847c1927d5
igorgcf <igorTecnologia@outlook.com>
2025-08-26 00:09:22 -0300

    Main

---
+++ Class11/Main.java
@@ -0,0 +1,16 @@
+package Class11;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        System.out.println("****--------------------------****");
+        student.toWalk();
+        student.Organize();
+        student.speak();
+        student.toTeach();
+        student.toCorrect();
+        System.out.println("****--------------------------****");
+
+    }
+}
