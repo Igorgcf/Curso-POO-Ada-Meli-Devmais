375fe0162d2417e7466050e13951bc1a7c0c547a
igorgcf <igorTecnologia@outlook.com>
2025-08-26 01:19:41 -0300

    Trocar Elementos Model

---
+++ exercicios/exercicio08/TrocarElementos.java
@@ -0,0 +1,17 @@
+package exercicios.exercicio08;
+
+public class TrocarElementos {
+
+    public static <T> void trocar(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static <T> void imprimirArray(T[] array) {
+        for (T elemento : array) {
+            System.out.print(elemento + " ");
+        }
+        System.out.println();
+    }
+}
