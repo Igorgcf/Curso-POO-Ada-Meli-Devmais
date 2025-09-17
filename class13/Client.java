95f111b522c32f0cc30530219f82a2a14323d014
Igorgcf <igorTecnologia@outlook.com>
2025-09-17 17:01:05 -0300

    Client Model

---
+++ class13/Client.java
@@ -0,0 +1,14 @@
+package class13;
+
+public class Client {
+
+    private PersonalData data;
+
+    public Client(PersonalData data) {
+        this.data = data;
+    }
+
+    public PersonalData getData() {
+        return data;
+    }
+}
