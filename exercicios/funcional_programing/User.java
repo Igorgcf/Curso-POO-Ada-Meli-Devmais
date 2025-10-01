6359cc02a34e0b61dc8fb12f26465f3a3c2d62e1
Igorgcf <igorTecnologia@outlook.com>
2025-10-01 17:22:01 -0300

    User Model

---
+++ exercicios/funcional_programing/User.java
@@ -0,0 +1,27 @@
+package exercicios.funcional_programing;
+
+public class User {
+
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
