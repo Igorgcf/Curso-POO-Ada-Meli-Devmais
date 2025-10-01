13fdbe2be1aa8d537d0708884cfe4a3a4099a37e
Igorgcf <igorTecnologia@outlook.com>
2025-10-01 17:23:20 -0300

    Main

---
+++ exercicios/funcional_programing/Main.java
@@ -0,0 +1,18 @@
+package exercicios.funcional_programing;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Exercises exercises = new Exercises();
+
+        exercises.doubleNumbers();
+        exercises.userNames();
+        exercises.filterNumbers();
+        exercises.filterUsersByAge();
+        exercises.reduceToNumbers();
+        exercises.oldestUser();
+        exercises.sumOfAges();
+
+    }
+}
